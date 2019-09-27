238a37ad2764081c3a73836091af0a762bf1b985
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2019-09-27 20:28:26 +0700

    handle multiple TC, add more TC

--- controllers/submitcode.js
+++ controllers/submitcode.js
@@ -1,39 +1,54 @@
+numberOfCasesDict = {
+    "TP-1 SDA 2019": 3
+}
+
 const handleSubmitCode = (fs, hackerEarthApi) => (req, resp) => {
     const { problemName, code } = req.body;
 
     let config = {};
-    config.time_limit = 1;
+    config.time_limit = 3;
     config.memory_limit = 323244;
     config.source = code;
     config.input = "";
     config.language = "JAVA";
 
-    // Test Case 1
-    fs.readFile("static/in1", 'utf8', (err, data) => {
-        if (err) {
-            resp.json("failed");
-        } else {
-            let inputData = data;
-            fs.readFile("static/out1", 'utf8', (err, data) => {
+    numberOfCases = numberOfCasesDict[problemName];
+    promises = []
+    for (let i = 0; i < numberOfCases; ++i) {
+        promises[i] = new Promise((resolve, reject) => {
+            fs.readFile(`static/${problemName}_in${i+1}`, 'utf8', (err, data) => {
                 if (err) {
-                    resp.json("failed");
+                    reject(new Error('failed'));
                 } else {
-                    let outputData = data;
-                    config.input = inputData;
-                    hackerEarthApi.run(config, (err, response) => {
+                    let inputData = data;
+                    fs.readFile(`static/${problemName}_out${i+1}`, 'utf8', (err, data) => {
                         if (err) {
-                            resp.json("failed");
+                            reject(new Error('failed'));
                         } else {
-                            resp.json({
-                                input: inputData,
-                                hasil: JSON.parse(response),
-                                output: outputData
-                            });
+                            let outputData = data;
+                            config.input = inputData;
+                            hackerEarthApi.run(config, (err, response) => {
+                                if (err) {
+                                    reject(new Error('failed'));
+                                } else {
+                                    resolve({
+                                        input: inputData,
+                                        hasil: JSON.parse(response),
+                                        output: outputData
+                                    });
+                                }
+                            })
                         }
-                    })
+                    });
                 }
             });
-        }
+        });
+    }
+
+    Promise.all(promises).then(values => {
+        resp.json(values);
+    }).catch(error => {
+        resp.json("failed");
     });
 }
 
